22ffba84320fbbfcd461826fa5eebe3251f21502
terence-line <terence_line@hotmail.fr>
2022-08-16 17:10:31 +0200

    ajout de commentaires

--- front/js/cart.js
+++ front/js/cart.js
@@ -60,17 +60,19 @@ else {
         for (i = 0; i < result.length; i++) {
 
                
-            // Ajout de l'element article.
+            // Ajout de l'element article 
             let cartArticles = document.createElement("article");
             cartItems.appendChild(cartArticles);
             cartArticles.setAttribute("data-id", "{product-ID}");
             cartArticles.setAttribute("data-color", "{product-color}");
             cartArticles.className = "cart__item";
 
-            // Ajout de l'element div qui va contenir l'img.
+
+            // Ajout de la div cart__item__img qui va contenir l'img.
             let divCartImages = document.createElement("div");
             divCartImages.className = "cart__item__img";
             cartArticles.appendChild(divCartImages);
+
 
             // Ajout de l'élement img.
             let cartImages = document.createElement("img");
@@ -78,45 +80,53 @@ else {
             cartImages.setAttribute('alt', result[i].altTxt);
             divCartImages.appendChild(cartImages);
 
-            // Ajout d'une div.
+
+            // Ajout de la  div cart__item__content après l'image
             let divCartItems = document.createElement("div");
             divCartItems.className = "cart__item__content";
             cartArticles.appendChild(divCartItems);
 
-            // Ajout d'une div.
+
+            // Ajout de la div cart__item__content
             let divCartItemsDescription = document.createElement("div");
             divCartItemsDescription.className = "cart__item__content__description";
             divCartItems.appendChild(divCartItemsDescription);
+
 
             // Ajout du h2 qui va contenir le nom du produit.
             let divCartItemsDescriptionName = document.createElement("h2");
             divCartItemsDescription.appendChild(divCartItemsDescriptionName);
             divCartItemsDescriptionName.textContent = result[i].name;
 
+
             // Ajout d'un p qui va contenir la couleur du produit.
             let divCartItemsDescriptionColor = document.createElement("p");
             divCartItemsDescription.appendChild(divCartItemsDescriptionColor);
             divCartItemsDescriptionColor.textContent = result[i].color;
 
-            // Ajout d'un p qui va contenir le prix du produit.
+
+            // Ajout d'un p qui va contenir le prix du produit récupéré avec la prommesse resolve
             let divCartItemsDescriptionPrice = document.createElement("p");
             divCartItemsDescription.appendChild(divCartItemsDescriptionPrice);
-            divCartItemsDescriptionPrice.textContent = result[i].price + " €"; // Ici le prix a été récupéré de l'api directement.
+            divCartItemsDescriptionPrice.textContent = result[i].price + " €"; 
 
-            // Ajout d'une div.
+
+            // Ajout de la div cart__item__content__settings
             let divCartItemsSetting = document.createElement("div");
             divCartItemsSetting.className = "cart__item__content__settings";
             divCartItems.appendChild(divCartItemsSetting);
 
-            // Ajout d'une div.
+            // Ajout de la div cart__item__content__settings__quantity
             let divCartItemsSettingQuantity = document.createElement("div");
             divCartItemsSettingQuantity.className = "cart__item__content__settings__quantity";
             divCartItemsSetting.appendChild(divCartItemsSettingQuantity);
+
 
             // Ajout d'un p qui va contenir le mot "Qté :".
             let divCartItemsSettingQty = document.createElement("p");
             divCartItemsSettingQuantity.appendChild(divCartItemsSettingQty);
             divCartItemsSettingQty.textContent = "Qté : ";
+
 
             // Ajout de l'input qui va contenir la quantité.
             let inputQuantity = document.createElement("input");
@@ -129,10 +139,11 @@ else {
             inputQuantity.setAttribute("name", "itemQuantity");
             
 
-            // Ajout d'une div.
+            // Ajout de la div cart__item__content__settings__delete
             let divCartItemsDelete = document.createElement("div");
             divCartItemsDelete.className = "cart__item__content__settings__delete";
             divCartItems.appendChild(divCartItemsDelete);
+            
 
             // Ajout d'un p qui va contenir le bouton "Supprimer".
             let pDeleteItem = document.createElement("p");
