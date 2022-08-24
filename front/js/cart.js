e4c05717a9ea243f8aafff77f864261cbc8ced39
terence-line <terence_line@hotmail.fr>
2022-08-24 11:55:59 +0200

    ajout message d'alerte -le panier est vide- et commentaires

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,177 +1,192 @@
+// Récupération des produits contenus dans le local storage
 const products = JSON.parse(localStorage.getItem("products"));
 
-function getPrice(product) {
+// Vérification du contenu du panier : si le panier est vide
+if (products === null) {
+    alert("Votre panier est  vide");
+}
 
-    return new Promise((resolve) => {
+// Si le panier est plein, afficher les produits
+else {
 
-        fetch(`http://localhost:3000/api/products/${product.id}`).then((response) => {
+    /* La méthode Promise.resolve(valeur) renvoit un objet Promise qui est résolu avec la valeur donnée.
+    Si cette valeur est une promesse, la promesse est renvoyée, si la valeur possède une méthode then, 
+    la promesse renvoyée « suivra » cette méthode et prendra son état ; 
+    sinon, la promesse renvoyée sera tenue avec la valeur.*/
 
-            return response.json();
+    function getPrice(product) {
+    
+        return new Promise((resolve) => {
 
-        }).then((result) => {
+            fetch(`http://localhost:3000/api/products/${product.id}`).then((response) => {
 
-            product.altTxt = result.altTxt;
+                 return response.json();
 
-            product.imageUrl = result.imageUrl;
+            }).then((result) => {
 
-            product.price = result.price;
+                product.altTxt = result.altTxt;
 
-            product.name = result.name;
+                product.imageUrl = result.imageUrl;
 
-            return resolve(product);
+                product.price = result.price;
+
+                product.name = result.name;
+
+                return resolve(product);
+
+            });
 
         });
 
-    });
+    }
 
-}
+    /* La méthode Promise.all()renvoit une promesse ( Promise) qui est résolue
+    lorsque l'ensemble des promesses retenues dans l'itérable passé en argument ont été résolues.*/
 
-Promise.all(products.map(getPrice)).then((result) => {
+    Promise.all(products.map(getPrice)).then((result) => {
 
-    // Ici tu ecris ton code
+        const totalQuantityNode = document.querySelector("#totalQuantity");
 
-    const totalQuantityNode = document.querySelector("#totalQuantity");
-
-    const totalPriceNode =  document.querySelector("#totalPrice");
+        const totalPriceNode =  document.querySelector("#totalPrice");
     
-    let html = "";
+        let html = "";
 
-    result.forEach((kanap)=>{
+        result.forEach((kanap)=>{
           
+            html+= `
 
-           html+= `
-
-               <article class="cart__item" data-id="${kanap.id}" data-color="${kanap.color}">
+                <article class="cart__item" data-id="${kanap.id}" data-color="${kanap.color}">
                     <div class="cart__item__img">
                     <img src="${kanap.imageUrl}" alt="${kanap.altTxt}">
                     </div>
                     <div class="cart__item__content">
                     <div class="cart__item__content__description">
-                         <h2>${kanap.name}</h2>
-                         <p>${kanap.color}</p>
-                         <p>${kanap.price} €</p>
+                        <h2>${kanap.name}</h2>
+                        <p>${kanap.color}</p>
+                        <p>${kanap.price} €</p>
                     </div>
                     <div class="cart__item__content__settings">
-                         <div class="cart__item__content__settings__quantity">
-                         <p>Qté :</p>
-                         <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${kanap.quantity}">
-                         </div>
-                         <div class="cart__item__content__settings__delete">
-                         <p class="deleteItem">Supprimer</p>
-                         </div>
+                        <div class="cart__item__content__settings__quantity">
+                        <p>Qté :</p>
+                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${kanap.quantity}">
+                        </div>
+                        <div class="cart__item__content__settings__delete">
+                        <p class="deleteItem">Supprimer</p>
+                        </div>
                     </div>
                     </div>
-               </article>
-           
-           `;
+                </article>
+                `;
 
-    });
+        });
 
-    function getTotalQuantity(arrayOfKanap){
+        // Fonction pour claculer le total des articles contenus dans le panier
+        function getTotalQuantity(arrayOfKanap){
 
-          let totalQuantity = 0;
+            let totalQuantity = 0;
 
-          arrayOfKanap.forEach((kanap)=>{
+            arrayOfKanap.forEach((kanap)=>{
 
                totalQuantity = Number(totalQuantity) + Number(kanap.quantity);
 
-          })
+            })
 
-          return  totalQuantity
+            return  totalQuantity;
 
-    }
+        }
 
+        // Fonction pour calculer le prix total du panier
+        function getTotalPrice(arrayOfKanap){
 
-    function getTotalPrice(arrayOfKanap){
+            let totalPrice = 0;
 
-          let totalPrice = 0;
+            arrayOfKanap.forEach((kanap)=>{
 
-          arrayOfKanap.forEach((kanap)=>{
+                totalPrice= Number(totalPrice) + Number(kanap.price * kanap.quantity); 
 
-               totalPrice= Number(totalPrice) + Number(kanap.price * kanap.quantity); 
+            })
 
-          })
+            return totalPrice; 
 
-          return totalPrice; 
+        }
 
-    }
+        document.querySelector("#cart__items").innerHTML = html;
 
-    document.querySelector("#cart__items").innerHTML = html;
-
-    totalQuantityNode.textContent = getTotalQuantity(result);
+        totalQuantityNode.textContent = getTotalQuantity(result);
     
-    totalPriceNode.textContent = getTotalPrice(result);
+        totalPriceNode.textContent = getTotalPrice(result);
 
-    const deleteItems = document.querySelectorAll(".deleteItem");
+        const deleteItems = document.querySelectorAll(".deleteItem");
 
-    deleteItems.forEach((item, indexItem)=>{
+        deleteItems.forEach((item, indexItem)=>{
 
-          item.addEventListener("click", ()=>{
+            item.addEventListener("click", ()=>{
 
-                  return deleteItem(item, result[indexItem]);
+                return deleteItem(item, result[indexItem]);
 
-          })
+            })
 
-    });
+        });
 
-    const itemsQuantity = document.querySelectorAll(".itemQuantity");
+        const itemsQuantity = document.querySelectorAll(".itemQuantity");
 
-    itemsQuantity.forEach((input, indexInput)=>{
+        itemsQuantity.forEach((input, indexInput)=>{
 
-          input.addEventListener("input", ()=>{
+            input.addEventListener("input", ()=>{
 
-                 updateQuantity(input, result[indexInput])
+                updateQuantity(input, result[indexInput])
 
-          })
+            })
 
-    })
+        })
 
+        // Fonction pour supprimer un article du panier
+        function deleteItem(itemToDelete, product){
 
-    function deleteItem(itemToDelete, product){
+            // On doit trouver l'index de l'élément qu'on veut supprimer dans le tableau de produits issus du localStorage.
 
-          // On doit trouver l'index de l'element qu'on veut supprimer dans le tableau de produits issus du localStorage
+            const deleteIndex = result.findIndex((element)=>{
 
-          const deleteIndex = result.findIndex((element)=>{
+                return element.color === product.color;
 
-               return element.color === product.color;
+            });
 
-          });
+            result.splice(deleteIndex, 1);
 
-          result.splice(deleteIndex, 1);
+            let localStorageCard = JSON.parse(localStorage.getItem("products"));
 
-          let localStorageCard = JSON.parse(localStorage.getItem("products"));
+            localStorageCard.splice(deleteIndex, 1);
 
-          localStorageCard.splice(deleteIndex, 1);
+            localStorage.setItem("products", JSON.stringify(localStorageCard));
 
-          localStorage.setItem("products", JSON.stringify(localStorageCard));
+            itemToDelete.closest(".cart__item").remove();
 
-          itemToDelete.closest(".cart__item").remove();
-
-          totalQuantityNode.textContent = getTotalQuantity(result);
+            totalQuantityNode.textContent = getTotalQuantity(result);
     
-          totalPriceNode.textContent = getTotalPrice(result);
+            totalPriceNode.textContent = getTotalPrice(result);
        
 
-    }
+        }
 
-    function  updateQuantity(input, product){
+        // Fonction pour modifier la quantité d'un produit
+        function  updateQuantity(input, product){
 
-          const updateIndex = result.findIndex((element)=>{
+            const updateIndex = result.findIndex((element)=>{
 
-               return element.color === product.color;
+                return element.color === product.color;
 
-          });
+            });
 
           
-          let kanapProduct = products[updateIndex];
+            let kanapProduct = products[updateIndex];
 
-          kanapProduct.quantity = input.value;
+            kanapProduct.quantity = input.value;
 
-          products[updateIndex] = kanapProduct;
+            products[updateIndex] = kanapProduct;
 
           
         
-    }
+        }
 
-});
-
+    });
+}
